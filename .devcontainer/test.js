a747b17f8ca21a0dd23a8ea5bf65741aaf2c98bf
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2023-11-02 10:26:32 +0000

    some new code

--- .devcontainer/test.js
+++ .devcontainer/test.js
@@ -1,1 +1,62 @@
 console.log("aman");
+//const its value are not changed
+// let its value are changed
+// var do not use beacuse block scope and functional scope
+// "use strict"; treate all js code as newer version
+// alert
+
+// data type 
+// string=""
+// int=3
+// boolean = true, false
+// null = standalone value
+// undefined
+// Symbol= unique
+console.log(typeof null); // object to chack which data type are in null,int etc.
+
+// Conversion
+
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+// "33" = 33
+// "33abc"= NaN
+// true = 1; false=0
+
+// ""= false
+// "aman"= true
+ 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// Operation
+let value= 3
+let negvalue = -value
+
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+console.log("--------------------------");
+let str1 = "hello"
+let str2 = "aman"
+let str3 = str1+str2;
+console.log(str3);
+console.log(2+2);
+console.log(1+"2");
+console.log("1"+2);
+console.log(1+2+"2");
+console.log("1"+2+2);
