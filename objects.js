9e155c09bcb88b95a1dd30399ee3d16c50841ecc
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2023-11-10 07:53:46 +0000

    object nested

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-// singleton :- it is created by constructor object and it is single object.ye constructor se jo banega wo singleton oject hota h.
+// singleton :- it is created by constructor object and it is single object. ye constructor se jo banega wo singleton oject hota h.
 // Object.create : created through constructor method.
 
 // object literals
@@ -19,18 +19,36 @@ const jsUser ={
 }
 
 // accesing the Object
-console.log(jsUser.email)
-console.log(jsUser["email"])
+// console.log(jsUser.email)
+// console.log(jsUser["email"])
 
-console.log(jsUser.full-name) // its worng
-console.log(jsUser["full-name"]) // its wright
-console.log(jsUser[mysym])// accesing the symble
-
-
-// changing the value of object
-jsUser.email="aman213@gmail.com"
-// lock the value
-Object.email=""
+// console.log(jsUser.full-name) // its worng
+// console.log(jsUser["full-name"]) // its wright
+// console.log(jsUser[mysym])// accesing the symble
 
 
+// // changing the value of object
+// jsUser.email="aman213@gmail.com"
+// // lock the value
+// Object.email=""
 
+
+const rguser={
+    email:"aman@gmail.com",
+    fullname:{
+        userfullname:{
+            fname:"aman",
+            lname:"kumar"
+        }
+    }
+}
+
+// console.log(rguser.fullname.userfullname.fname)//nested object useing
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+// const obj3={obj1,obj2} // it will print the like nasted object 
+const obj3 =Object.assign({},obj1,obj2) // {} this sing will marge the all multipale object
+console.log(obj3);
