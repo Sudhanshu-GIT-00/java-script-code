4b65f85cbb3d465f7af924fc648194fa23cf1b84
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2023-11-24 12:12:21 +0000

    some

--- 6.js
+++ 6.js
@@ -1,7 +1,20 @@
-const coding = ["js","ruby","python","java","cpp"]
+// const coding = ["js","ruby","python","java","cpp"]
 
-const value = coding.forEach((item)=>{
-    console.log(item);
-    return item
+// const value = coding.forEach((item)=>{
+//     console.log(item);
+//     return item
+// })
+// console.log(values);
+
+// const mynums =[1,2,3,4,,5,6,7,8,9,10]
+
+// let  newNums = mynums.filter((num)=> num > 4)
+//  console.log(newNums);
+
+
+const mynums =[1,2,3,4,,5,6,7,8,9,10]
+
+let  newNums = mynums.filter((num)=> {
+    return num > 4
 })
-console.log(values);
+ console.log(newNums);
