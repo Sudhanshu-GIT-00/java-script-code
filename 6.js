de5058523623f319ed4baa6da7c526b325a4a63d
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2023-11-24 12:18:27 +0000

    6.js

--- 6.js
+++ 6.js
@@ -12,9 +12,16 @@
 //  console.log(newNums);
 
 
-const mynums =[1,2,3,4,,5,6,7,8,9,10]
+// const mynums =[1,2,3,4,,5,6,7,8,9,10] 
 
-let  newNums = mynums.filter((num)=> {
-    return num > 4
-})
- console.log(newNums);
+// let  newNums = mynums.filter((num)=> {
+//     return num > 4
+// })
+// const newnum = []
+// mynums.forEach((num)=>{
+//     if(num > 4){
+//         newnum.push(num)
+//     }
+// })
+//  console.log(newNums);
+
