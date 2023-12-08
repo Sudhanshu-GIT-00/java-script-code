2dabcaca7164fac3a731e6300eb6a05b543f068b
Sudhanshu Ranjan <rsudhanshu620@gmail.com>
2023-12-08 12:22:26 +0000

    complate BMI Calculate

--- BMI-Calc/index.js
+++ BMI-Calc/index.js
@@ -8,20 +8,30 @@ form.addEventListener('submit', function (event_object_name){
     const height = parseInt(document.querySelector('#height').value);
     const weight = parseInt(document.querySelector('#weight').value);
     const result = document.querySelector('#results');
+    const weight_guide = document.querySelector('#weight-guide');
 
     if(height === '' || height < 0 || isNaN(height)){
         results.innerHTML = `plz give valid height ${height}`;
 
     }
-    else if(weight === '' || weight < 0 || isNaN(weight)){
+    if(weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = `plz give valid weight ${weight}`;
 
     }
     else{
-       const BMI = (weight / ((height * height)/10000)).toFixed(2);
+        const BMI = (weight / ((height * height)/10000)).toFixed(2);
        //show the result
        result.innerHTML = `<span>${BMI}</span>`;
-
+       
+       if(BMI <= 18.6){
+        weight_guide.innerHTML = `</br><span>Under Weight</span>`;
+       }
+       else if(BMI > 18.6 && BMI >= 24.9){
+        weight_guide.innerHTML = `</br><span>Normal Weight</span>`;
+       }
+       if(BMI < 34.9){
+        weight_guide.innerHTML = `</br><span>Over weight</span>`;
+       }
     }
     
 });
